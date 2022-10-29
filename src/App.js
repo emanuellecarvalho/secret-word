45d00e771da091a899b6f519989b0ed85bc24394
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-10-29 12:59:09 -0300

    feat: create Home component and add css style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Secret Word</h1>
+      <Home />
     </div>
   );
 }
