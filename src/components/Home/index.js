ce9ba781d7c90ecaa3758d9e6e6effc2f311433d
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-11-02 16:04:18 -0300

    feat: add Home and Game components style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ const Home = ({ startGame }) => {
   return (
     <div className='start'>
       <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para começar a jogar</p>
+      <p>Clique no botão abaixo para começar a jogar!</p>
       <button onClick={startGame}>Começar o jogo</button>
     </div>
   )
