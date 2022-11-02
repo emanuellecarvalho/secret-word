1008e6a54318b335aa10676597f0b0fd7925ca0c
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-11-02 09:18:57 -0300

    feat: add methods to pick category, words and letters

--- src/App.js
+++ src/App.js
@@ -19,9 +19,35 @@ const stages = [
 const App = () => {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
- // const [gameStage, setGameStage] = useState(stages[0].name);
+
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+    console.log(word);
+
+    return {word, category};
+
+  };
   // start the game 
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.toLowerCase().split('');
+
+    console.log(word, category);
+    console.log(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
@@ -42,6 +68,6 @@ const App = () => {
       {gameStage === 'end' && <GameOver retry={retry} /> }
     </div>
   );
-}
+};
 
 export default App;
