fe4ec97e436d8b170feb4743b83fa8bc947fcc3a
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-11-02 09:23:41 -0300

    feat: set the states of category, word and wordLetters

--- src/App.js
+++ src/App.js
@@ -48,6 +48,12 @@ const App = () => {
     console.log(word, category);
     console.log(wordLetters);
 
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+
     setGameStage(stages[1].name);
   };
 
